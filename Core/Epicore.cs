8a69eeb432a366a251677b9d8462c74280ec2dbc
E-gy <balakhelisey@gmail.com>
2019-02-28 21:55:12 +0100

    Main components

--- Core/Epicore.cs
+++ Core/Epicore.cs
@@ -6,6 +6,16 @@ using System.Linq;
 
 namespace Epicoin {
 
+	internal abstract class MainComponent {
+
+		protected readonly Epicore core;
+
+		protected MainComponent(Epicore core) => this.core = core;
+
+		internal abstract void InitAndRun();
+
+	}
+
 	internal class InterThreadComms<M> where M : ITCMessage  {
 
 		protected ConcurrentQueue<M> messages = new ConcurrentQueue<M>();
