be311c374c3859dca0a50e9a83a1577f99fa4bba
E-gy <balakhelisey@gmail.com>
2019-03-08 06:53:12 +0100

    NPcP testing 1 - refl instantitation

---
+++ Core.Tests/NPcPTest.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Epicoin {
+
+	[TestFixture]
+	public class NPcPProblemWrapperTest {
+
+		[Test]
+		public void Instantiation(){
+			NPcProblemWrapper wrapper = new NPcProblemWrapper(new InefficientIntFactProblem());
+			Assert.IsNotNull(wrapper, "Instatiation failed");
+		}
+
+		public class InefficientIntFactProblem : INPcProblem<int, List<int>> {
+
+			public List<int> solve(int i){
+				int nextFact(int pf){
+					for(int nf = pf; nf <= i; nf++) if(pf != 1 && i % nf == 0){ i /= nf; return nf; }
+					return 1;
+				}
+				List<int> facts = new List<int>();
+				while(i > 1) facts.Add(nextFact(facts.Count > 0 ? facts.Last() : 1));
+				return facts;
+			}
+
+			public bool check(int i, List<int> facts) => facts.Aggregate(1, (p, f) => p*f) == i;
+
+		}
+
+	}
+
+}
