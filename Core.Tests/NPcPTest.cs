b253cb0cd5f1d3eb06f26c552d8484a99c939bc2
E-gy <balakhelisey@gmail.com>
2019-03-08 07:33:24 +0100

    Testing delegation & ser/deser

--- Core.Tests/NPcPTest.cs
+++ Core.Tests/NPcPTest.cs
@@ -15,6 +15,26 @@ namespace Epicoin {
 			Assert.IsNotNull(wrapper, "Instatiation failed");
 		}
 
+		[Test]
+		public void Delegation(){
+			NPcProblemWrapper wrapper = new NPcProblemWrapper(new InefficientIntFactProblem());
+			const string p1 = "{\"o\":242}";
+
+			string s1 = wrapper.solve(p1);
+			Assert.AreEqual("{\"o\":[2,11,11]}", s1, "Solving failed - unexpected result");
+			Assert.True(wrapper.check(p1, s1), "Solution check [242] failed - false negative");
+
+			Random random = new Random();
+			for(int i = 0; i < 100; i++){
+				int n = random.Next(25, 12500);
+				if(n == 242) continue;
+				string p2 = "{ o: " + n + " }";
+				string s2 = wrapper.solve(p2);
+				Assert.True(wrapper.check(p2, s2), "Solution [rand] solve/check failed");
+				Assert.False(wrapper.check(p1, s2), "Solution [rand] check failed - false positive (against 242)");
+			}
+		}
+
 		public class InefficientIntFactProblem : INPcProblem<int, List<int>> {
 
 			public List<int> solve(int i){
