883845f5f734f2774912bdcff70839a6f7101829
Ashwin Sridhar <ashwin.sridhar-kamakshi@live.in>
2019-03-11 10:32:09 +0100

    Update Program.cs

--- Core/Program.cs
+++ Core/Program.cs
@@ -5,13 +5,18 @@ namespace Epicoin {
 	class Program {
 		static void Main(string[] args){
 			Epicore core = new Epicore();
+
 			Console.WriteLine("Starting up...");
 			core.Start();
+
 			Console.WriteLine("Start up successful.");
+
 			Console.WriteLine("Press any key to exit.");
+
 			while(!Console.KeyAvailable) Thread.Yield();
 			Console.WriteLine("Shutting down...");
 			core.Stop();
+
 			Console.WriteLine("Shutdown complete.");
 		}
 	}
