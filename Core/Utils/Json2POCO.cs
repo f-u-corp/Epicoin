5b929cf817dae4c0c1827d5aad7c61509c0f0c71
E-gy <balakhelisey@gmail.com>
2019-04-18 14:01:05 +0200

    Constructing classes at runtime!

---
+++ Core/Utils/Json2POCO.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Epicoin.Core {
+
+	internal class GroupedClassesBuilder {
+
+		readonly AssemblyName assembly;
+		readonly AssemblyBuilder assemblyBuilder;
+		readonly ModuleBuilder moduleBuilder;
+
+		public GroupedClassesBuilder(string assembly){
+			this.assembly = new AssemblyName(assembly);
+			this.assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(this.assembly, AssemblyBuilderAccess.Run);
+			this.moduleBuilder = assemblyBuilder.DefineDynamicModule(this.assembly.Name);
+		}
+
+		public CBuilder Class(string name, TypeAttributes attr = TypeAttributes.Class, Type parent = null) => new CBuilder(this, name, attr | TypeAttributes.AnsiClass, parent);
+
+		public CBuilder Struct(string name, TypeAttributes attr = 0) => Class(name, attr | TypeAttributes.Sealed | TypeAttributes.ExplicitLayout | TypeAttributes.Serializable, typeof(ValueType));
+
+		internal class CBuilder {
+
+			readonly GroupedClassesBuilder group;
+			readonly string name;
+
+			readonly TypeBuilder builder;
+
+			internal CBuilder(GroupedClassesBuilder group, string name, TypeAttributes attr, Type parent){
+				this.group = group;
+				this.name = name;
+				this.builder = group.moduleBuilder.DefineType(name, attr, parent);
+			}
+
+			public CBuilder Field(string name, Type type, FieldAttributes attr = FieldAttributes.Public, CustomAttributeBuilder cattr = null){
+				var f = builder.DefineField(name, type, attr);
+				if(cattr != null) f.SetCustomAttribute(cattr);
+				return this;
+			}
+
+			public Type Build() => builder.CreateType();
+
+		}
+
+	}
+
+}
