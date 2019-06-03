2e4434acda4d35341c6433214c520f51dd4c06cb
E-gy <balakhelisey@gmail.com>
2019-06-03 15:55:44 +0200

    EFOBE requests need respective treatment on the reply sidr

--- Core/EFOBE.cs
+++ Core/EFOBE.cs
@@ -153,6 +153,14 @@ namespace Epicoin.Core {
 				}
 			}
 
+			internal class EFOBESendRequest : ITM {
+				public readonly FileInfo cacheEFOBEHere;
+
+				public EFOBESendRequest(FileInfo cache){
+					this.cacheEFOBEHere = cache;
+				}
+			}
+
 		}
 
 	}
